94157e46dab70de96fd11e1dbe30de70d363a1d5
Yoda <mihael.peric@hotmail.com>
2019-10-13 17:48:55 +0200

    routes

--- api/routes/get/get_route.py
+++ api/routes/get/get_route.py
@@ -1,6 +1,6 @@
 USER = '/v1/user'
 SERVE_FILE = '/v1/dir/<path:path>'
-COUNTRIES = '/countries'
+COUNTRIES = '/v1/countries'
 CITIES_BY_COUNTRY = '/v1/countries/<int:country_id>/cities'
 USER_NEWS = '/v1/news'
 
