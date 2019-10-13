46cffbf61877028351fc59c18083f6c3430a2ad5
Yoda <mihael.peric@hotmail.com>
2019-10-13 15:53:57 +0200

    new commit

--- api/model/config.py
+++ api/model/config.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://mihael:Mihael0110.@localhost/e_wallet?use_unicode=1&charset=utf8mb4'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://Mihael:Mihael0110.@localhost:5432/e_wallet'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://nuunnhgldwiswb:d3786174762426b0f7516ab5679e88144b4e9c16bb9b37582c866461b5d1d635@ec2-54-217-225-16.eu-west-1.compute.amazonaws.com:5432/deomvf9srj5c3d'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://nuunnhgldwiswb:d3786174762426b0f7516ab5679e88144b4e9c16bb9b37582c866461b5d1d635@ec2-54-217-225-16.eu-west-1.compute.amazonaws.com:5432/deomvf9srj5c3d'
 
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = Flask
