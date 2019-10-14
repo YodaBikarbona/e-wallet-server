779e3ca4f16e4ebb7831419e0a61aa155301da88
Yoda <mihael.peric@hotmail.com>
2019-10-14 12:32:27 +0200

    change city

--- app.py
+++ app.py
@@ -140,11 +140,16 @@ def restart_password_endpoint():
 def add_new_city():
 
     from api.model.user import City
-    city = City()
-    city.created = now()
-    city.country_id = request.json['country_id']
-    city.name = request.json['name']
-    db.session.add(city)
+
+    city = City.query.filter(City.id == 2).first()
+
+    city.name = "Sarajevo"
+
+    # city = City()
+    # city.created = now()
+    # city.country_id = request.json['country_id']
+    # city.name = request.json['name']
+    # db.session.add(city)
     db.session.commit()
 
     return "True"
