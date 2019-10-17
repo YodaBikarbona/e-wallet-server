2a878572312088a70d82e0a25d54325b7d0e1487
Yoda <mihael.peric@hotmail.com>
2019-10-17 15:21:30 +0200

    fix date format

--- api/helper/helper.py
+++ api/helper/helper.py
@@ -159,9 +159,11 @@ def date_format(date, string=False, graph=False, birth_day=False, register=False
             date_list = date.split('-')
             date = "{0}-{1}-{2}".format(date_list[0], date_list[2], date_list[1])
             date = datetime.strptime(date, "%Y-%d-%m") + timedelta(days=1)
+            return datetime.strftime(date, "%Y-%m-%d")
         else:
             print(date)
-            date = datetime.strptime(date, "%Y-%d-%m")
+            date = datetime.strptime(date, "%Y-%m-%d") + timedelta(days=1)
+            return datetime.strftime(date, "%Y-%m-%d")
         return datetime.strftime(date, "%Y-%m-%d")
     date = "{0}".format(date[0])
     date = datetime.strptime(date, "%Y-%m-%d")
