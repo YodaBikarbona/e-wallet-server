ac1099191077a8818695cbba8ea42da848c978bb
Yoda <mihael.peric@hotmail.com>
2019-10-17 17:55:19 +0200

    heroku commit

--- api/model/providers/user.py
+++ api/model/providers/user.py
@@ -32,7 +32,7 @@ class UserProvider:
         user.code = random_string(6)
         user.salt = new_salt()
         user.password = new_psw(user.salt, user_data['password'])
-        user.role_id = 1
+        user.role_id = role.id
         #user.image_id = None
         #user.image_id = 1 if user_data['gender'] == 'male' else user.image_id
         #user.image_id = 2 if user_data['gender'] == 'female' else user.image_id
