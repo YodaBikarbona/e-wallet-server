aaba497a3f5d49287562540b317bb38b533d11a8
Yoda <mihael.peric@hotmail.com>
2019-10-17 16:36:12 +0200

    change db url

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ from flask_mail import Mail
 app = Flask(__name__, static_url_path='')
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://mihael:Mihael0110.@localhost/e_wallet?use_unicode=1&charset=utf8mb4'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://Mihael:Mihael0110.@localhost:5432/e_wallet'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://nuunnhgldwiswb:d3786174762426b0f7516ab5679e88144b4e9c16bb9b37582c866461b5d1d635@ec2-54-217-225-16.eu-west-1.compute.amazonaws.com:5432/deomvf9srj5c3d'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://rbcqpbieptqppr:1f4a0f21dfb985eb26ae4d53104c65bf4e411ef4d126dad459367291d36bc79b@ec2-46-137-173-221.eu-west-1.compute.amazonaws.com:5432/d8nfn34d0uogfn'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = Flask
 
 PROJECT_HOME = os.path.dirname(os.path.realpath(__file__))
