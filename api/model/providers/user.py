f9df5a234a4ab7f9105c89c3321f5f077e5a648b
Yoda <mihael.peric@hotmail.com>
2019-10-18 18:01:01 +0200

    date_format

--- api/model/providers/user.py
+++ api/model/providers/user.py
@@ -36,9 +36,12 @@ class UserProvider:
         #user.image_id = None
         #user.image_id = 1 if user_data['gender'] == 'male' else user.image_id
         #user.image_id = 2 if user_data['gender'] == 'female' else user.image_id
-        db.session.add(user)
-        db.session.commit()
-        return user
+
+
+        # db.session.add(user)
+        # db.session.commit()
+        # return user
+        return True
 
     @classmethod
     def get_user_by_ID(cls, user_id):
