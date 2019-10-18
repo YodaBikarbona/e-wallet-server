8dfc83b390bc30b7604a365c116700881b45833b
Yoda <mihael.peric@hotmail.com>
2019-10-18 17:26:16 +0200

    wkhtmltopdf for heroku

--- api/views/bill.py
+++ api/views/bill.py
@@ -253,7 +253,8 @@ def print_pdf_report(request):
     scss = ['static/report/report.scss']
     rendered = render_template("report_template.html", user=user, items=items, report_date=date_format(now()),
                                bills=bills, bill_type=request.json['billType'], currencies=currencies, summ=summ_list)
-    report = pdfkit.from_string(rendered, '/app/report.pdf', css=scss, configuration=_get_pdfkit_config())
+    print(rendered)
+    report = pdfkit.from_string(rendered, 'out.pdf', css=scss, configuration=_get_pdfkit_config())
     #report = pydf.generate_pdf(html=rendered)
     response = make_response(report)
     response.headers['Content-Type'] = 'application/pdf'
