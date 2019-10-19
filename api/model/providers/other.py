490ee5d599daa62c7f78235b1fc3a3e564d757b0
Yoda <mihael.peric@hotmail.com>
2019-10-19 21:17:11 +0200

    connection for cities and countries

--- api/model/providers/other.py
+++ api/model/providers/other.py
@@ -7,12 +7,12 @@ class OtherProvider:
     @classmethod
     def get_countries(cls):
         countries = Country.query.filter(Country.activated == True).all()
-        db.session.close()
+        #db.session.close()
         return countries
 
     @classmethod
     def get_cities(cls, country_id):
         cities = City.query.filter(City.country_id == country_id).all()
-        db.session.close()
+        #db.session.close()
         return cities
 
