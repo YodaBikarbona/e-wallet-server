2b2d87bff0da1a04683fbdbcf10971519d270b83
Yoda <mihael.peric@hotmail.com>
2019-10-20 16:52:33 +0200

    deleted sessions

--- api/model/providers/bill.py
+++ api/model/providers/bill.py
@@ -1,6 +1,6 @@
 from api.model.bill import Bill, BillCategory, BillSubCategory
 from api.model.country import Currency
-from api.model.config import db, Session
+from api.model.config import db
 
 
 class BillProvider:
